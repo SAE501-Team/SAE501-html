b5ee86272cfabf8905079461599c750e2c23de65
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2025-01-21 16:03:05 +0100

    update: behhchat module

--- modules/behhchat/behhchat.php
+++ modules/behhchat/behhchat.php
@@ -21,13 +21,13 @@ class BehhChat extends Module
 
     public function install()
     {
-        return parent::install() && 
-               $this->registerHook('actionCustomerAccountAdd') && 
-               $this->registerHook('actionAuthentication') &&
-               $this->registerHook('actionCustomerLogoutAfter') &&
-               $this->registerHook('ModuleRoutes');
+        return parent::install() &&
+            $this->registerHook('actionCustomerAccountAdd') &&
+            $this->registerHook('actionAuthentication') &&
+            $this->registerHook('actionCustomerLogoutAfter') &&
+            $this->registerHook('ModuleRoutes');
     }
-    
+
     public function uninstall()
     {
         return parent::uninstall();
@@ -45,7 +45,7 @@ class BehhChat extends Module
                 'content' => json_encode($data),
             ],
         ];
-        
+
         $context = stream_context_create($options);
 
         // Try/Catch to handle API call errors
@@ -57,6 +57,19 @@ class BehhChat extends Module
             PrestaShopLogger::addLog('API call failed: ' . $e->getMessage(), 3);
         }
     }
+
+    // Generate random UUID
+    protected function generateUuid4() {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+
 
     // Register
     public function hookActionCustomerAccountAdd($params)
@@ -77,25 +90,40 @@ class BehhChat extends Module
     public function hookActionAuthentication($params)
     {
         $customer = $params['customer'];
-    
+
         $data = [
+            'id' => $customer->id,
             'email' => $customer->email,
-            'password' => $customer->passwd,
+            // 'idRoom' => $this->generateUuid4()
         ];
-    
+
         $this->sendDataToExpress($data, 'api/login');
+
+        $cookieValue = json_encode($data);
+
+        // Créer un cookie nommé 'behhchat_data', qui expire dans 1 heure
+        setcookie('behhchat_data', $cookieValue, time() + 3600 * 4, "/", "", false, false);
     }
+
 
     // Logout
     public function hookActionCustomerLogoutAfter($params)
     {
         $customer = $params['customer'];
-    
+
         $data = [
-           'email' => $customer->email, // Email de l'utilisateur
+            'email' => $customer->email, // Email de l'utilisateur
         ];
-    
+
         // Envoie une requête à l'API pour gérer la déconnexion
         $this->sendDataToExpress($data, 'api/logout');
-    }
+
+        // Remove le cookie behhchat_data
+        if (isset($_COOKIE['behhchat_data'])) {
+            unset($_COOKIE['behhchat_data']);
+            setcookie('behhchat_data', '', -1, '/');
+            return true;
+        } else {
+            return false;
+        }    }
 }
